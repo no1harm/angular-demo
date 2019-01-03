e0fcfcbc6f7e69633c7495743a94d4588714bb55
no1harm <chen09869@gmail.com>
2019-01-03 19:09:58 +0800

    使用input 接收 hero 信息

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.sass']
 })
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero: Hero;
   constructor() { }
 
   ngOnInit() {
