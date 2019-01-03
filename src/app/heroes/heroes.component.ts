c027a92d5f05c4a3f216db0c06e833d4c84da680
no1harm <chen09869@gmail.com>
2019-01-03 20:17:11 +0800

    使用Rxjs获取数据

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,8 +17,11 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
-
 }
