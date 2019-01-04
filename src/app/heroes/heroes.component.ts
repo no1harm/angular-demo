56fa78507c8be26722fd3b9634e2ecdbe2062e1b
no1harm <chen09869@gmail.com>
2019-01-04 10:36:39 +0800

    通过路由信息获取id

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,15 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
+  // selectedHero: Hero;
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
   // getHeroes(): void {
   //   this.heroes = this.heroService.getHeroes();
   // }
